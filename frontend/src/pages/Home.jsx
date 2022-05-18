6f9934eac0a1ccebaa288c6603e459bf6dac242a
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-05-18 17:48:54 +0200

    add form on the Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import '../assets/css/Home.css';
 import { GlobalContext } from '../context/GlobalContext';
 
 function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <form className='form-container'>
+        <label>Title :</label>
+        <input type='text' />
+
+        <label>Description :</label>
+        <input type='textarea' />
+      </form>
+    </div>
+  );
 }
 
 export default Home;

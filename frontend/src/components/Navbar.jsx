cbc570b9bc09e75be8681d2e93815c51d505f791
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-05-18 16:49:54 +0200

    edit css file for the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/img/logo2.png';
 
+import '../assets/css/Navbar.css';
+
 export default function Navbar() {
+  const [menu, setMenu] = useState(false);
+  const changeMenu = () => {
+    setMenu(!menu);
+  };
   return (
-    <div>
+    <div className='navbar-container'>
       <img className='img-logo' src={logo} alt='logo' />
+      <h1>
+        Things <span className='memory'>Memory</span>
+      </h1>
+      <button className='btn-menu' onClick={changeMenu}></button>
+      <div className="menu-container">
+
+      </div>
     </div>
   );
 }

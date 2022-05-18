0412d7e405f0f23329429f28563f3d5163bf18e7
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-05-18 20:42:08 +0200

    add package circular progress bar

--- frontend/src/components/Pomodoro.jsx
+++ frontend/src/components/Pomodoro.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { CircularProgressBar } from '@tomik23/react-circular-progress-bar';
 
 export default function Pomodoro() {
-  return (
-    <div>Pomodoro</div>
-  )
+  return <div>Pomodoro</div>;
 }

b77845994e7a696c8e1998ecdae8acf2e5aa6aec
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-05-18 15:45:39 +0200

    create original context

--- frontend/src/context/GlobalContext.jsx
+++ frontend/src/context/GlobalContext.jsx
@@ -1,1 +1,33 @@
 import React, { createContext, useState } from 'react';
+
+export const GlobalContext = createContext();
+
+function GlobalContextProvider({ children }) {
+  const [originTask, setOriginTask] = useState([
+    {
+      id: uuidv4(),
+      title: 'Faire les courses',
+      description: 'Allez a intermarché pour des courses',
+    },
+    {
+      id: uuidv4(),
+      title: 'Faire le ménage',
+      description: 'Ne pas oublier de jeter les poubelles',
+    },
+    {
+      id: uuidv4(),
+      title: 'Faire un tour de moto',
+      description: 'Ballade de quelques kilometres.',
+    },
+  ]);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        task: [originTask, setOriginTask],
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+}
